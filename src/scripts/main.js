133788b80a827d0550caa3ed7d7c20d16d06fa47
Lauren Riddle <lari7132@gmail.com>
2019-11-01 13:36:38 -0500

    create arrays to store artist and create factory functions to create artists

---
+++ src/scripts/main.js
@@ -0,0 +1,43 @@
+const JumpStop = []
+const ChattenRecords = []
+const PolarRecords = []
+
+const createFunkArtist = (name, genre, age) => {
+    return {
+        name: name,
+        genre: genre,
+        age: age
+    }
+}
+
+const createRapArtist = (name, genre, age) => {
+    return {
+        name: name,
+        genre: genre,
+        age: age
+    }
+}
+
+const createCountryArtist = (name, genre, age) => {
+    return {
+        name: name,
+        genre: genre,
+        age: age
+    }
+}
+
+const createBluegrassArtist = (name, genre, age) => {
+    return {
+        name: name,
+        genre: genre,
+        age: age
+    }
+}
+
+const createPopArtist = (name, genre, age) => {
+    return {
+        name: name,
+        genre: genre,
+        age: age
+    }
+}
