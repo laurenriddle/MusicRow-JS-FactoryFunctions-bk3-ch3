e466a16f576fa8448ec48225e542ae40a72c3644
Lauren Riddle <lari7132@gmail.com>
2019-11-01 13:49:07 -0500

    invoked the factory functions and added the new objects to the correct arrays

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
+// arrays to hold the artists once they are created
 const JumpStop = []
 const ChattenRecords = []
 const PolarRecords = []
 
+// below are functions to create artists for each genre of music
 const createFunkArtist = (name, genre, age) => {
     return {
         name: name,
@@ -41,3 +43,24 @@ const createPopArtist = (name, genre, age) => {
         age: age
     }
 }
+
+// invoking functions to create artists
+const BruceAtikins = createCountryArtist("Bruce Atikins", "Country", 23)
+const JensenBrown = createPopArtist("Jensen Brown", "Pop", 20)
+const DreFunkz = createFunkArtist("Dre Funkz", "Funk", 25)
+const DustaGrimes = createRapArtist("Dusta Grimes", "Rap", 21)
+const BartholomewDanielson = createBluegrassArtist("Bartholomew Danielson", "Bluegrass", 23)
+const AvileeDallas = createCountryArtist("Avilee Dallas", "Country", 19)
+const AustinKinkaid = createPopArtist("Austin Kinkaid", "Pop", 22)
+const LoyoncéBranis = createRapArtist("Loyoncé Branis", "Rap", 27)
+
+
+
+// adding artist ot the correct array
+ChattenRecords.push(BruceAtikins, BartholomewDanielson, AvileeDallas)
+PolarRecords.push(JensenBrown, AustinKinkaid)
+JumpStop.push(DreFunkz, DustaGrimes, LoyoncéBranis)
+
+console.log(ChattenRecords)
+console.log(PolarRecords)
+console.log(JumpStop)
